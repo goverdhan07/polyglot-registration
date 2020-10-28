b436b7d36ad827437849eab5f31db046148a6ba6
Goverdhan Biradar <66107302+goverdhan07@users.noreply.github.com>
2020-10-29 00:41:38 +0530

    updated the code
    Hi just gave a try on this code.
    Thanks <3
    This is my first time contributing to open source. Excited :)

--- src/csharp/PolyglotRegistration/Validators/EmailValidator.cs
+++ src/csharp/PolyglotRegistration/Validators/EmailValidator.cs
@@ -1,9 +1,17 @@
-﻿namespace PolyglotRegistration.Validators
+using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace PolyglotRegistration.Validators
 {
     public class EmailValidator : IValidator<string>
     {
         public bool IsValid(string inputValue)
         {
+            string email = txtemail.Text;
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            if (match.Success)
+                lbl_message.Text=email + " is Valid Email Address";  
             return true;
         }
     }
